e25588de93e8b4da28f39de8fac2cffe7ff6fe05
Renat <alridcom@gmail.com>
2023-06-24 14:26:29 +0300

    add homework

---
+++ lesson_1/HW/4/Program.cs
@@ -0,0 +1,10 @@
+﻿// задача 8
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()!);
+
+int count = 2;
+while (count <= a)
+{
+    Console.Write(count);
+    count = count + 2;
+}
