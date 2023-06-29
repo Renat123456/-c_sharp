7dbff7b5861182273f69aaea8f971723e77302c5
Renat <alridcom@gmail.com>
2023-06-29 22:21:14 +0300

    lesson 2" git add .

---
+++ lesson_3/33/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int count = 1;
+if (a == 0 )
+{
+ Console.WriteLine("1");
+}
+else {
+while (count <= Math.Abs(a) )
+        {
+            Console.WriteLine(Math.Pow(count, 2));
+             count++;
+        }
+}
