60fd5fc51e697d4a093bcb4a0c0873558021aa48
Renat <alridcom@gmail.com>
2023-07-06 22:51:40 +0300

    lesson 5

---
+++ lesson_5/HW/1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void Quantity(int[] arr)
+{
+    int count = 0;  
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int a = arr[i] % 2;
+        if (a == 0) count += 1;        
+    }
+    Console.WriteLine($"Количество чётных чисел в массиве -> {count}");
+}
+
+void Fill(int[] arr, int a, int b)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(a, b + 1);
+    }
+}
+
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();     
+}
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите начало периода");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конец периода");
+int B = int.Parse(Console.ReadLine()!);
+
+int [] arr = new int[size];
+Fill(arr, A, B);
+PrintArr(arr);
+Quantity(arr);
---
+++ lesson_5/HW/3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+void Difference(double[] arr)
+{
+    double min = arr[0];
+    double max = arr[0];
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (min > arr[i]) min = arr[i];
+        if (max < arr[i]) max = arr[i];      
+    }
+    Console.WriteLine($"Минимум -> {min}, максимум -> {max}");
+    Console.WriteLine($"Разница между максимальным и минимальным элементом массива -> {Math.Round(max - min, 2)}");
+}
+
+void Fill(double[] arr, int a, int b)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(new Random().NextDouble() + new Random().Next(a, b), 2);
+    }
+}
+
+void PrintArr(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();   
+}
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите начало периода");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конец периода");
+int B = int.Parse(Console.ReadLine()!);
+
+double [] arr = new double[size];
+Fill(arr, A, B);
+PrintArr(arr);
+Difference(arr);
