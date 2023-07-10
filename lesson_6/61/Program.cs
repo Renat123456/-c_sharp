0e60599d19ff5430f52cb915e0e7192499ca93d3
Renat <alridcom@gmail.com>
2023-07-10 22:42:42 +0300

    lesson 6

---
+++ lesson_6/61/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Напишите программу, которая принимает на вход три числа
+//    и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+
+bool Triangle(int a, int b, int c)
+{
+    if (a < b + c && b < c + a && c < a + b)
+    return true;
+    else 
+    return false;
+}
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Triangle(a, b, c));
---
+++ lesson_6/62/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Напишите программу, которая будет
+//    преобразовывать десятичное число в двоичное.
+
+
+string BinaryNumber(int num)
+{
+    string number = string.Empty;
+
+    for (; num != 0; num /= 2)
+    {
+        number = num % 2 + number;
+    }
+    return number;
+}
+
+System.Console.WriteLine(BinaryNumber(12));
