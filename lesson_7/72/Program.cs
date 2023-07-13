12003dcdd57a673686dee574f4998ffdcdc58023
Renat <alridcom@gmail.com>
2023-07-13 22:23:56 +0300

    lesson 7

---
+++ lesson_7/72/Program.cs
@@ -0,0 +1,27 @@
+﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+
+void Sum_massive(int[,] array)
+{
+    int sum = 0;
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("не будет ровной диагонали");
+        return;
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            if (i == j)
+            {
+                sum = sum + array[i, j];
+            }
+
+
+        }
+    }
+    Console.Write($"{sum}");
+
+}
---
+++ lesson_7/HW/2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void FillArr(int[,] arr, int a, int b)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(a, b + 1);
+        }        
+    }
+}
+
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();         
+    }
+    Console.WriteLine();     
+}
+
+Console.WriteLine("Введите размер строк массива");
+int row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите размер столбцов массива");
+int column = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите начало периода");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конец периода");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите номер строки");
+int R = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите номер столбца");
+int C = int.Parse(Console.ReadLine()!);
+
+int [,] arr = new int[row, column];
+FillArr(arr, A, B);
+PrintArr(arr);
+
+if (R <= arr.GetLength(0) && C <= arr.GetLength(1))
+{
+    Console.WriteLine(arr[R -1 , C - 1]);    
+}
+else
+{
+    Console.WriteLine("Такой позиции нет");    
+}
