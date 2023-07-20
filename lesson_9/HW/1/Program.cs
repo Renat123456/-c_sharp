c6ed2c5d674d82cc2615a101a6d95c07a708590d
Renat <alridcom@gmail.com>
2023-07-20 22:52:06 +0300

    lesson 9

---
+++ lesson_9/HW/1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+Console.WriteLine($"Введите число M");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Введите число N");
+int n = int.Parse(Console.ReadLine()!);
+
+void PrintNumber(int M, int N)
+{
+    if (N < M)
+     return;
+    
+    PrintNumber(M, N - 1);
+    if (N % 2 == 0) Console.Write($"{N} ");    
+}
+PrintNumber(m, n);
---
+++ lesson_9/HW/2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.WriteLine($"Введите число M");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Введите число N");
+int n = int.Parse(Console.ReadLine()!);
+
+int Sum(int M, int N)
+{
+    int sum = 0;
+    if (N < M)
+    {        
+        return sum;
+    }    
+    return Sum(M, N - 1) + (sum += N);
+}
+Console.WriteLine(Sum(m, n));
